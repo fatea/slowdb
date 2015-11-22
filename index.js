a35c7a8e49addd927c38b1111ec105c3d66be501
wjt <wjt128@gmail.com>
2015-11-22 21:24:34 +0800

    improve the basic functions

--- index.js
+++ index.js
@@ -44,112 +44,131 @@ class slow{
     }
 
 
-    delete(){
-        function deleteImages(req){
-            var item = req.body;
-            //var isSaveAction = (req.baseUrl == '/save');//如果baseUrl 是 /save, 则需要分裂.否则不需要
+    find(id, col){
+    //well, I don't think there should be any async find...
+    }
 
-            // ImageSet.items[item.id] = item;
 
-            var temp_item =  ImageSet.items.find(function (element) {
-                return element.id === item.id;
+    findSync(id, col){
+        if(col == undefined){
+            return null;
+        }else {
+            var temp_item =  this[col].find(function (element) {
+                return element['id'] === id;
+            });
+
+            if(temp_item != undefined){
+                return temp_item;
+            }else {
+                return null;
+            }
+        }
+    }
+
+
+
+    insert(obj, col) {
+        if(col == undefined){
+            return Q.fcall(function () {
+                throw new Error("the col is needed");
+            });
+        }else{
+            if(obj.id == undefined){
+                return Q.fcall(function(){
+                    throw new Error('the obj does not have an id');
+                })
+            }
+
+            var temp_item =  this[col].find(function (element) {
+                return element['id'] ===obj.id;
             });
 
 
-            if(temp_item != undefined){//如果找到的话,先删后加
-                // console.log('这是找到的item: '+ temp_item.id);
-                let index = ImageSet.items.indexOf(temp_item);
-                ImageSet.items.splice(index, 1);
+            if(temp_item == undefined){
+                this[col].unshift(obj);
 
-
-
-
-
-
-
-
-
-
-
-
+                return this.save();
             }else {
                 return Q.fcall(function () {
                     throw new Error("the file does not exist");
                 });
             }
-
-            // ImageSet.items.unshift(item);
-            //console.log(ImageSet);
-
-            //ImageSet.key = "new value";
-
-
-
-
-            /*
-
-             var buffer;
-             var data = JSON.stringify(ImageSet, null, 2);
-             console.log('data is ' + data);
-             buffer = new Buffer(data, 'utf8');
-             */
-            //var promise = writeFile(realPathOfImageSetJson+'imageSet.json', buffer);
-
-
-
-
-            var promise = writeJson(realPathOfImageSetJson+'imageSet.json', ImageSet);
-            return promise.then(
-                function (success) {
-
-                    let promises = [];
-
-                    promises.push(splitImageSetAndSaveBy20Albums(ImageSet, item.id));
-                    promises.push(deleteSingleAlbum(item));
-
-                    return Q.all(promises).then(
-                        function(success){
-                            return {
-                                status: true,
-                                id: item.id
-                            };
-                        },
-                        function(error){
-                            throw new Error('error happens when saving files');
-                        }
-                    );
-
-
-                },
-                function (error){
-                    console.log(error);
-
-                    throw new Error('error happens when saving '+item.id, item.id);
-                }
-            );
-            /*
-             return promise.then(
-             function(success){
-             return {
-             status: true,
-             id: newsFile.metaData.id
-             };
-             },
-             function (error) {
-             console.log(error);
-
-             throw new Error('error happens when saving '+newsFile.name, newsFile.id);
-             }
-             );
-
-             */
         }
 
     }
 
-    search(){
+
+    insertSync(obj, col){
 
     }
+
+    update(id, obj, col){
+            if(col == undefined){
+
+                return Q.fcall(function () {
+                    throw new Error("the col is needed");
+                });
+
+        }else{
+
+            var temp_item =  this[col].find(function (element) {
+                return element['id'] === id;
+            });
+
+
+            if(temp_item != undefined){//如果找到的话,先删后加
+                // console.log('这是找到的item: '+ temp_item.id);
+                let index = this[col].indexOf(temp_item);
+                this[col].splice(index, 1);
+                this[col].unshift(obj);
+
+                return this.save();
+            }else {
+                return Q.fcall(function () {
+                    throw new Error("the file does not exist");
+                });
+            }
+        }
+
+
+    }
+
+    updateSync(id, col, key, value){
+
+    }
+
+
+    delete(id, col){
+        if(col == undefined){
+            var temp =  this[id];
+            if(temp != undefined){//如果找到的话,先删后加
+                delete this[id];
+            }else {
+                return Q.fcall(function () {
+                    throw new Error("the file does not exist");
+                });
+            }
+        }else{
+
+            var temp_item =  this[col].find(function (element) {
+                return element[key] === key;
+            });
+
+
+            if(temp_item != undefined){//如果找到的话,先删后加
+                // console.log('这是找到的item: '+ temp_item.id);
+                let index = this[col].indexOf(temp_item);
+                this[col].splice(index, 1);
+            }else {
+                return Q.fcall(function () {
+                    throw new Error("the file does not exist");
+                });
+            }
+    }
+    }
+
+
+
 
 
 
